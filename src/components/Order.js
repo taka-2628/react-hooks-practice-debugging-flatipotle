95bf1c5805ee98358bd843632505f2345972bcba
Takashi <th2826@columbia.edu>
2022-01-06 17:42:08 -0500

    Completed assignment

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import burritoImg from "../images/burrito-bowl.jpg";
+import Side from "./Side";
 
 function Order(props) {
   const [isClicked, setIsClicked] = useState(false);
 
   function handleClick() {
-    isClicked = !isClicked;
+    setIsClicked(!isClicked);
   }
 
   return (
@@ -35,9 +36,9 @@ function Order(props) {
         ) : (
           <p>No sides</p>
         )}
-
+        
         {/* this is just a shortcut to writing state.isClicked ? <Side sides={props.sides} /> : null */}
-        {state.isClicked && <Side sides={props.sides} />}
+        {isClicked && <Side sides={props.sides} />}
       </div>
     </div>
   );
